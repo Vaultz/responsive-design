ea205ae06112cb3a4600cd7e30aec2358634ba2e
Vaultz <paulvialart@hotmail.fr>
2017-04-12 16:27:24 +0200

    Setting the desktop page

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById('closing_cross').addEventListener("click", function removeHeadband() {
+  document.getElementById('headband').parentNode.removeChild(document.getElementById("headband"));
+});
