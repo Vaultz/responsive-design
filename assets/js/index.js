6c0039b2265dac428db6fbd0a0735ca99875f9da
Vaultz <paulvialart@hotmail.fr>
2017-04-13 15:55:02 +0200

    Fixing minor discrepancies

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// header event
+document.getElementById('closing_cross').addEventListener("click", function removeHeadband() {
+  document.getElementById('headband').parentNode.removeChild(document.getElementById("headband"));
+  document.getElementById('main').style.height = ('94vh');
+});
+
+var clientWidth = document.documentElement.clientWidth;
+if (clientWidth > 600) {
+  document.getElementById('burgerbutton').parentNode.removeChild(document.getElementById('burgerbutton'));
+}
